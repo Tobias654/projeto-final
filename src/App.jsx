080ccdee695d5b634ebab0c832ef8e8b44b58457
Tobias654 <tobias.mouraa10@gmail.com>
2022-09-12 00:46:31 -0300

    alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-import { useState, useEffect } from 'react'
-import CardsMagic from './components/CardsMagic'
-import api from './services/apim'
+import React from "react";
+import { View } from "react-native";
+import Main from "./src/screens/Main/Main";
 
-function App() {
-    const [cards, setCards] = useState([])
-  
-  
-    const getCards = async (total) => {
-      const listCards = []
-  
-      for(let i = 1; i <= total; i++) {
-        const newCard = await api
-          .get(`${i.toString()}`)
-          .then(response => response.data)
-          .catch(err => console.log(err))
-  
-  
-        listCards.push(newCard)  
-      }
-  
-      setCards([...cards, ...listCards])
-  
-      console.log(cards)  
-    }
-  
-    useEffect(() => {
-      getCards()
-    }, [])
-  
-    return (
-      <div className="App container">
-        <h1>Cards de Magic</h1>
-        <div className="cards">
-          {magic?.map(magic => {
-            return <CardsMagic card={magic} />
-          })}
-        </div>
-      </div>
-    )
-  }
-  
-  export default App
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Main key="main" />
+    </View>
+  );
+}
--- src/services/apim.jsx
+++ src/services/apim.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apim = axios.create({
-    baseURL: "https://api.magicthegathering.io/v1/cards"
+    baseURL: "https://api.magicthegathering.io/v1/cards",
+    
 })
 
 export default apim
